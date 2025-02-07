063c42bfbdcfc3a68c8cfef1c13c870e212a76f6
Igor Quintaes <striter.alfa+git@gmail.com>
2025-02-07 18:59:00 -0300

    add some tests

--- BotDeScans.UnitTests/Specs/Features/Publish/Steps/04_PdfFilesStepTests.cs
+++ BotDeScans.UnitTests/Specs/Features/Publish/Steps/04_PdfFilesStepTests.cs
@@ -1,7 +1,14 @@
 ﻿using AutoFixture;
+using BotDeScans.App.Features.Publish;
 using BotDeScans.App.Features.Publish.Steps;
+using BotDeScans.App.Services;
+using BotDeScans.UnitTests.Extensions;
 using BotDeScans.UnitTests.FakeObjects;
+using FakeItEasy;
 using FluentAssertions;
+using FluentResults;
+using FluentResults.Extensions.FluentAssertions;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace BotDeScans.UnitTests.Specs.Features.Publish.Steps;
@@ -12,6 +19,8 @@ public class PdfFilesStepTests : UnitTest
 
     public PdfFilesStepTests()
     {
+        fixture.FreezeFake<FileService>();
+        fixture.FreezeFake<FileReleaseService>();
         fixture.Inject(PublishStateBuilder.Create(fixture, StepEnum.PdfFiles));
         step = fixture.Create<PdfFilesStep>();
     }
@@ -29,16 +38,78 @@ public class PdfFilesStepTests : UnitTest
 
     public class ValidateBeforeFilesManagementAsync : PdfFilesStepTests
     {
+        [Fact]
+        public async Task ShouldReturnSuccess()
+        {
+            var result = await step.ValidateBeforeFilesManagementAsync(cancellationToken);
 
+            result.Should().BeSuccess();
+        }
     }
 
     public class ValidateAfterFilesManagementAsync : PdfFilesStepTests
     {
+        [Fact]
+        public async Task ShouldReturnSuccess()
+        {
+            var result = await step.ValidateAfterFilesManagementAsync(cancellationToken);
 
+            result.Should().BeSuccess();
+        }
     }
 
     public class ExecuteAsync : PdfFilesStepTests
     {
+        public ExecuteAsync()
+        {
+            var scopedDirectory = fixture.Create<string>();
+            var pdfDirectory = fixture.Create<string>();
 
+            A.CallTo(() => fixture
+                .FreezeFake<FileReleaseService>()
+                .CreateScopedDirectory())
+                .Returns(scopedDirectory);
+
+            A.CallTo(() => fixture
+                .FreezeFake<FileService>()
+                .CreatePdfFileAsync(
+                    fixture.Freeze<PublishState>().ReleaseInfo.ChapterNumber,
+                    fixture.Freeze<PublishState>().InternalData.OriginContentFolder,
+                    scopedDirectory))
+                .Returns(Result.Ok(pdfDirectory));
+        }
+
+        [Fact]
+        public async Task GivenSuccessfulExecutionShouldReturnSuccessResult()
+        {
+            var result = await step.ExecuteAsync(cancellationToken);
+
+            result.Should().BeSuccess();
+        }
+
+        [Fact]
+        public async Task GivenSuccessfulExecutionShouldSetZipFilePath()
+        {
+            fixture.Freeze<PublishState>().InternalData.PdfFilePath = null!;
+
+            await step.ExecuteAsync(cancellationToken);
+
+            fixture.Freeze<PublishState>().InternalData.PdfFilePath.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public async Task GivenErrorToCreazeZipShouldReturnFailResult()
+        {
+            const string ERROR_MESSAGE = "some error.";
+
+            A.CallTo(() => fixture
+                .FreezeFake<FileService>()
+                .CreatePdfFileAsync(A<string>.Ignored, A<string>.Ignored, A<string>.Ignored))
+                .Returns(Result.Fail(ERROR_MESSAGE));
+
+            var result = await step.ExecuteAsync(cancellationToken);
+
+            result.Should().BeFailure().And.HaveError(ERROR_MESSAGE);
+        }
     }
 }
