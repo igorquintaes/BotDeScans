3cf3b957b80d45c0dc55cfa8b7aff690ef5131a6
Igor Quintaes <striter.alfa+git@gmail.com>
2025-06-08 21:38:15 -0300

    change title create interaction code structure

---
+++ BotDeScans.App/Features/Titles/Create/Handler.cs
@@ -0,0 +1,49 @@
+﻿using BotDeScans.App.Extensions;
+using BotDeScans.App.Infra;
+using BotDeScans.App.Models.Entities;
+using BotDeScans.App.Services.Discord;
+using FluentResults;
+using FluentValidation;
+using Remora.Discord.API.Abstractions.Objects;
+
+namespace BotDeScans.App.Features.Titles.Create;
+
+public class Handler(
+    DatabaseContext databaseContext,
+    RolesService rolesService,
+    IValidator<Title> validator)
+{
+    public virtual async Task<Result> ExecuteAsync(
+        string name,
+        string role,
+        CancellationToken cancellationToken)
+    {
+        var roleResult = await GetDiscordRole(role, cancellationToken);
+        if (roleResult.IsFailed)
+            return roleResult.ToResult();
+
+        var title = new Title { Name = name, DiscordRoleId = roleResult.Value?.ID.Value };
+        var validationResult = await validator.ValidateAsync(title, cancellationToken);
+        if (validationResult.IsValid is false)
+            return validationResult.ToResult();
+
+        await databaseContext.AddAsync(title, cancellationToken);
+        await databaseContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Ok();
+    }
+
+    private async Task<Result<IRole?>> GetDiscordRole(
+        string roleRequest, 
+        CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(roleRequest))
+            return Result.Ok<IRole?>(null);
+
+        var roleResult = await rolesService.GetRoleFromGuildAsync(roleRequest, cancellationToken);
+
+        return roleResult.IsSuccess 
+            ? Result.Ok<IRole?>(roleResult.Value) 
+            : roleResult.ToResult();
+    }
+}
--- BotDeScans.App/Features/Titles/Create/Interactions.cs
+++ BotDeScans.App/Features/Titles/Create/Interactions.cs
@@ -1,11 +1,4 @@
 ﻿using BotDeScans.App.Builders;
-using BotDeScans.App.Extensions;
-using BotDeScans.App.Infra;
-using BotDeScans.App.Models.Entities;
-using BotDeScans.App.Services.Discord;
-using FluentResults;
-using FluentValidation;
-using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.Commands.Feedback.Services;
 using Remora.Discord.Interactivity;
 using Remora.Results;
@@ -14,10 +7,8 @@ using System.ComponentModel;
 namespace BotDeScans.App.Features.Titles.Create;
 
 public class Interactions(
-    DatabaseContext databaseContext,
-    FeedbackService feedbackService,
-    RolesService rolesService,
-    IValidator<Title> validator) : InteractionGroup
+    Handler handler,
+    FeedbackService feedbackService) : InteractionGroup
 {
     public const string MODAL_NAME = "Titles.Create.Interaction";
 
@@ -27,43 +18,11 @@ public class Interactions(
         string name,
         string role)
     {
-        // todo: o fluxo vai chamar duas vezes a api do discord aqui para roles,
-        // uma vez para obter o id através de uma string (nome ou id)
-        // outra vez para realizar a validação do objeto Title,
-        // pois tem validação compartilhada com create/edit/publish.
-        // O ideal aqui é criarmos regras do que validar ou não,
-        // E inicialmente podemos, em complemento, criar um cache das roles.
+        var result = await handler.ExecuteAsync(name, role, CancellationToken);
+        var embed = result.IsSuccess
+            ? EmbedBuilder.CreateSuccessEmbed("Obra cadastrada com sucesso!")
+            : EmbedBuilder.CreateErrorEmbed(result);
 
-        var roleResult = await GetDiscordRoleId(role);
-        if (roleResult.IsFailed)
-            return await feedbackService.SendContextualEmbedAsync(
-                embed: EmbedBuilder.CreateErrorEmbed(roleResult.ToResult()),
-                ct: CancellationToken);
-
-        var title = new Title { Name = name, DiscordRoleId = roleResult.Value?.ID.Value };
-        var validationResult = await validator.ValidateAsync(title);
-        if (validationResult.IsValid is false)
-            return await feedbackService.SendContextualEmbedAsync(
-                embed: EmbedBuilder.CreateErrorEmbed(validationResult.ToResult()),
-                ct: CancellationToken);
-
-        await databaseContext.AddAsync(title, CancellationToken);
-        await databaseContext.SaveChangesAsync(CancellationToken);
-
-        return await feedbackService.SendContextualEmbedAsync(
-            embed: EmbedBuilder.CreateSuccessEmbed("Obra cadastrada com sucesso!"),
-            ct: CancellationToken);
-    }
-
-    private async Task<FluentResults.Result<IRole?>> GetDiscordRoleId(string roleRequest)
-    {
-        if (string.IsNullOrWhiteSpace(roleRequest))
-            return FluentResults.Result.Ok<IRole?>(null);
-
-        var roleResult = await rolesService.GetRoleFromGuildAsync(roleRequest, CancellationToken);
-        if (roleResult.IsFailed)
-            return roleResult.ToResult();
-
-        return FluentResults.Result.Ok<IRole?>(roleResult.Value);
+        return await feedbackService.SendContextualEmbedAsync(embed: embed, ct: CancellationToken);
     }
 }
