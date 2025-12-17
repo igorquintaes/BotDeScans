9d43c9217a50147e7567885f8614d92295bc4a0f
Igor Quintaes <striter.alfa+git@gmail.com>
2025-12-16 22:31:57 -0300

    improve code

--- BotDeScans.App/Extensions/ConfigurationExtensions.cs
+++ BotDeScans.App/Extensions/ConfigurationExtensions.cs
@@ -12,14 +12,22 @@ public static class ConfigurationExtensions
 
     public static Result<T> GetRequiredValueAsResult<T>(this IConfiguration configuration, string key)
     {
-        var value = configuration.GetValue<string?>(key, null);
+        var value = configuration.GetValue<string?>(key);
+
         if (string.IsNullOrWhiteSpace(value))
             return Result.Fail($"'{key}' config value not found.");
 
-        return Result.Try(
-            () => (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFrom(value)!,
-             _ => new Error($"'{key}' config value contains a not supported value."));
-
+        try
+        {
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            var convertedValue = (T)converter.ConvertFromInvariantString(value)!;
+            return Result.Ok(convertedValue);
+        }
+        catch (Exception)
+        {
+            var errorMessage = $"'{key}' config value contains an unsupported value.";
+            return Result.Fail(errorMessage);
+        }
     }
 
     public static T[] GetValues<T>(this IConfiguration configuration, string key) =>
