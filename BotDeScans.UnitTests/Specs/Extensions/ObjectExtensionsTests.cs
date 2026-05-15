6b27562bafdd9a041b175323189c3740a5dd3d6b
Igor Quintaes <striter.alfa+git@gmail.com>
2026-05-15 14:53:44 -0300

    chore: add missing test

--- BotDeScans.UnitTests/Specs/Extensions/ObjectExtensionsTests.cs
+++ BotDeScans.UnitTests/Specs/Extensions/ObjectExtensionsTests.cs
@@ -1,6 +1,9 @@
 ﻿using BotDeScans.App.Extensions;
+using BotDeScans.App.Services.Logging;
 using FluentResults;
+using Serilog.Events;
 using System.Linq.Expressions;
+using ILogger = Serilog.ILogger;
 
 namespace BotDeScans.UnitTests.Specs.Extensions;
 
@@ -90,6 +93,44 @@ public abstract class ObjectExtensionsTests : UnitTest
             public static Task<Result> FailureMethodAsync(string errorMessage) =>
                 Task.FromResult(Result.Fail(errorMessage));
 
+            public static Task<Result> ThrowExceptionMethodAsync() =>
+                throw new InvalidOperationException("Test exception");
+        }
+    }
+
+    public sealed class SafeCallAsyncWithLogging : ObjectExtensionsTests, IDisposable
+    {
+        private readonly ILogger fakeLogger;
+
+        public SafeCallAsyncWithLogging()
+        {
+            fakeLogger = A.Fake<ILogger>();
+            A.CallTo(() => fakeLogger.IsEnabled(A<LogEventLevel>.Ignored)).Returns(true);
+
+            Result.Setup(cfg => cfg.Logger = new ResultLogger(fakeLogger));
+        }
+
+        public void Dispose() =>
+            Result.Setup(cfg => { });
+
+        [Fact]
+        public async Task GivenExceptionWhenLogIfFailedIsCalledShouldLogExceptionDetailsThroughResultLogger()
+        {
+            var testObject = fixture.Create<TestObject>();
+            Expression<Func<TestObject, Task<Result>>> expression = obj => TestObject.ThrowExceptionMethodAsync();
+
+            var result = await testObject.SafeCallAsync(expression);
+            result.LogIfFailed();
+
+            A.CallTo(fakeLogger)
+                .Where(call => call.Method.Name == nameof(ILogger.Write)
+                    && call.Arguments.Count >= 1
+                    && Equals(call.Arguments[0], LogEventLevel.Error))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        private class TestObject
+        {
             public static Task<Result> ThrowExceptionMethodAsync() =>
                 throw new InvalidOperationException("Test exception");
         }
