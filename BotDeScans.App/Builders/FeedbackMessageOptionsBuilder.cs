cec5ea6779718ced34d58016f4eb899d2f03c091
Igor Quintaes <striter.alfa+git@gmail.com>
2026-05-20 09:44:22 -0300

    chore: use variable naming convention in builder field

--- BotDeScans.App/Builders/FeedbackMessageOptionsBuilder.cs
+++ BotDeScans.App/Builders/FeedbackMessageOptionsBuilder.cs
@@ -9,14 +9,14 @@ public class FeedbackMessageOptionsBuilder
 {
     public const int MAX_ATTACHMENTS_ALLOWED = 10;
 
-    public static readonly string maxAttachmentsError = $"Discord allows only {MAX_ATTACHMENTS_ALLOWED} attachments for each message.";
+    public static readonly string MaxAttachmentsError = $"Discord allows only {MAX_ATTACHMENTS_ALLOWED} attachments for each message.";
 
     private readonly List<OneOf<FileData, IPartialAttachment>> attachments = [];
 
     public FeedbackMessageOptionsBuilder WithAttachment(string name, Stream stream)
     {
         if (attachments.Count >= MAX_ATTACHMENTS_ALLOWED)
-            throw new ArgumentOutOfRangeException(nameof(name), maxAttachmentsError);
+            throw new ArgumentOutOfRangeException(nameof(name), MaxAttachmentsError);
 
         var fileData = new FileData(name, stream);
         var fileAttachment = OneOf<FileData, IPartialAttachment>.FromT0(fileData);
